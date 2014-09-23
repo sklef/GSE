5769bbf8e672572ae30ee427f425315d73aa0dc9
d.s.smolyakov <d.s.smolyakov@gmail.com>
2014-09-23 18:01:07 +0400

    Add orthogonalization

--- @lgse/train.m
+++ @lgse/train.m
@@ -44,9 +44,6 @@ if this.iLogger.level < logLevel.Info
   this.iLogger.debug('Min number of neighbors: %d', min(sum(this.kernels ~= 0)));
   this.iLogger.debug('Maximum number of neighbors: %d', max(sum(this.kernels ~= 0)));
 end
-if min(sum(this.kernels > 0)) < this.reducedDimension * 2
-  disp('There are lonely points. Try to change parameters');
-end
 %% Compression Jacobian calculation
 this.iLogger.info('Tangent space alignment')
 
@@ -115,28 +112,98 @@ else
     for index = 1:this.sampleSize
         this.projectionJacobians{index} = this.localPCs{index};
     end
-    maxIterations = 10;
-    for currentDim = 1:this.reducedDimension
-      this.calculateJacobianComponent(currentDim, this.kernels, maxIterations);
-      this.updatePCs(currentDim);
-      this.updateVs(currentDim);
+    maxIterations = 100;
+    for currentDim = 1:(this.reducedDimension - 1)
+      this.calculateJacobianComponent(1, this.kernels, maxIterations);
+      for index = 1:this.sampleSize
+        this.localPCs{index} = findOrthogonalCompliment(this.localPCs{index}, ...
+           this.projectionJacobians{index}(:, currentDim));
+      end
     end
     %% just to plot delta
-    for multistartIteration = 1:length(this.historyDelta{1})
-      plot(0:maxIterations, log10(this.historyDelta{1}{multistartIteration}), '-*r');
-      hold on
-    end
-    plot(0:maxIterations, log10(this.historyDelta{2}{1}), '-*b');
+    plot(0:maxIterations, log10(this.historyDelta), '-*')
     xlabel('iteration')
     ylabel('log10 Delta')
     %%
-    for pointIndex = 1:this.sampleSize
-      vTv{pointIndex} = this.vs{pointIndex}' * this.vs{pointIndex};
-      diagonalLinearSpaceProjections{pointIndex} = eyeReducedDimension*sum(this.kernels(pointIndex, :));
+    for index = 1:this.sampleSize
+        this.vs{index} = eye(this.reducedDimension)
+        vTv{index} = eye(this.reducedDimension)
+        this.localPCs{index} = this.projectionJacobians{index}
     end
 end
 
+
+    
+%else
+%  options.disp = 0;
+%  options.isreal = 1;
+%  options.issym = 1;
+%  if this.newNormalization
+%    phi = phi2 - phi1;
+%    phi0 = phi0/sum(sum(phi0));
+%  else
+%    phi = phi0-phi1;
+%    phi0 = phi0/sum(sum(phi0));
+%  end
+%  alignmentMatrix = zeros(this.sampleSize * reducedDimension, reducedDimension);
+%  for dim = 1:reducedDimension
+%    if dim > 1
+%      orthCond = cell(this.sampleSize,1);
+%      for pointIndex = 1:this.sampleSize
+%        vi = alignmentMatrix((pointIndex - 1) * reducedDimension + 1:pointIndex * reducedDimension, dim - 1);
+%        orthCond{pointIndex} = vi * vi';
+%      end
+%      phi = phi + this.RegularizationLambda * blkdiag(orthCond{:});
+%    end
+%    [alignmentMatrix(:, dim), ~] = eigs(phi, 1, 'SA', options);% W = {v_i}|i=1,n
+%    for pointIndex = 1:this.sampleSize
+%      vi = alignmentMatrix((pointIndex - 1) * reducedDimension + 1:pointIndex * reducedDimension, dim);
+%      if (sqrt(sum(vi.^2)) > 0)
+%        vi = vi./(sqrt(sum(vi.^2)) + eps);
+%      end
+%      alignmentMatrix((pointIndex - 1) * reducedDimension + 1:pointIndex * reducedDimension, dim) = vi;
+%      orthCond{pointIndex} = vi * vi';
+%    end
+%  end
+%  for pointIndex = 1:this.sampleSize
+%    this.vs{pointIndex} = alignmentMatrix((pointIndex - 1) * reducedDimension + 1:pointIndex * reducedDimension, :); % v_i
+%    if this.newNormalization
+%      vTv{pointIndex} = this.vs{pointIndex}' * this.localEigenVals{pointIndex} ^ 2 * this.vs{pointIndex};
+%      this.projectionJacobians{pointIndex} = this.localPCs{pointIndex} * this.localEigenVals{pointIndex} * this.vs{pointIndex}; % H(X_i)
+%    else
+%      this.projectionJacobians{pointIndex} = this.localPCs{pointIndex} * this.vs{pointIndex}; % H(X_i)
+%      vTv{pointIndex} = eyeReducedDimension;%this.vs{pointIndex}' * this.vs{pointIndex};
+%    end
+%  end
+%end
+
+% for pointIndex1 = 1:this.sampleSize
+%   v = this.projectionJacobians{pointIndex1};
+%   cnt = 0;
+%   example = v;
+%   maxDist = 0;
+%   for pointIndex2 = 1:this.sampleSize
+%     if this.kernels(pointIndex1, pointIndex2) > 0,
+%       vi = this.projectionJacobians{pointIndex2};
+%       cnt = cnt + (-1)^(norm(vi - v) > norm(v));
+%       if maxDist < norm(vi - v)
+%         example = vi{pointIndex2};
+%         maxDist = norm(vi - v);
+%       end
+%     end
+%   end
+%   if cnt < 0
+%     Q = example'*v;
+%     Q = Q ./ repmat(sqrt(sum(Q.^2)), size(Q,1), 1);
+%     this.projectionJacobians{pointIndex1} = v * Q;
+%   end
+% end
+
+
 %% Compression calculation
+% this.constructCompressedSpace();
+% return
+
 this.iLogger.info('Embedding')
 % Solving linear system
 LHS = cell(this.sampleSize);
@@ -147,6 +214,10 @@ for pointIndex1 = 1:this.sampleSize
   LHSdiag = zeros(this.reducedDimension);
   for pointIndex2 = [1:pointIndex1-1 pointIndex1+1:this.sampleSize]
     tmp = this.kernels(pointIndex1, pointIndex2) * (vTv{pointIndex1} + vTv{pointIndex2});
+    size(vTv{1})
+    this.kernels(pointIndex1, pointIndex2) 
+    
+    size(LHSdiag)
     LHS{pointIndex1, pointIndex2} = tmp;
     LHSdiag = LHSdiag - tmp;
     RHStmp = RHStmp + this.kernels(pointIndex1, pointIndex2) * ...
@@ -157,25 +228,16 @@ for pointIndex1 = 1:this.sampleSize
   RHS{pointIndex1} = RHStmp;
 end
 
-% compressedPoints = reshape([cell2mat(LHS); repmat(eye(this.reducedDimension), 1, this.sampleSize)] \ ...
-%   [cell2mat(RHS); zeros(this.reducedDimension, 1)], this.reducedDimension, this.sampleSize);
+ compressedPoints = reshape([cell2mat(LHS); repmat(eye(this.reducedDimension), 1, this.sampleSize)] \ ...
+   [cell2mat(RHS); zeros(this.reducedDimension, 1)], this.reducedDimension, this.sampleSize);
 
 
-compressedPoints = reshape([cell2mat(LHS); cell2mat(diagonalLinearSpaceProjections)'] \ ...
-  [cell2mat(RHS); zeros(this.reducedDimension, 1)], this.reducedDimension, this.sampleSize);
-%% iterative approach 
-% this.iLogger.info('Embedding')
-% maxIterations = 100;
-% this.constructCompressedSpace(this.kernels, maxIterations); 
-% figure();
-% plot(0:maxIterations, log10(this.historyDeltaCompression), '-*r')
-% hold on
-% xlabel('iteration')
-% ylabel('log10 Delta(h)')
+%compressedPoints = reshape([cell2mat(LHS); cell2mat(diagonalLinearSpaceProjectionsInitial)] \ ...
+%  [cell2mat(RHS); zeros(this.reducedDimension, 1)], this.reducedDimension, this.sampleSize);
 
 %% Postprocessing
-[this.compressedTrainPoints, this.mappingSettingsReducedDimension] = mapminmax(compressedPoints); % this.compressedTrainPoints
-% this.compressedTrainPoints = this.compressedTrainPoints';
+[this.compressedTrainPoints, this.mappingSettingsReducedDimension] = mapminmax(compressedPoints);
+
 
 % for pointIndex = 1:this.sampleSize
 %   this.compressedTrainPoints(:, pointIndex) = pinv(this.vs{pointIndex1}) * this.compressedTrainPoints(:, pointIndex);
